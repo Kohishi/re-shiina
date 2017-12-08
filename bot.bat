ffa52ea99523b4832ac1b176b0e7a97f826f0b8e
Kohishi <32710349+Kohishi@users.noreply.github.com>
2017-12-08 21:19:39 +0530

    Update bot.bat

--- bot.bat
+++ bot.bat
@@ -5,21 +5,21 @@ pushd %~dp0
 
 %SYSTEMROOT%\py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO attempt
-%SYSTEMROOT%\py.exe -3 bot.py
+%SYSTEMROOT%\py.exe -3 selfbot.py
 PAUSE
 GOTO end
 
 :attempt
 py.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO lastattempt
-py.exe -3 bot.py
+py.exe -3 selfbot.py
 PAUSE
 GOTO end
 
 :lastattempt
 python.exe --version > NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 GOTO message
-python.exe bot.py
+python.exe selfbot.py
 PAUSE
 GOTO end
 
